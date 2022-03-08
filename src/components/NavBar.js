2d1dc93ffc1553e40db3b21d4571a84ed6776008
Chun Yi <8.5by11@gmail.com>
2022-03-08 16:58:57 -0500

    wip

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,117 @@
-import React from "react";
+import React, { useState } from 'react';
 
-function NavBar (){
+function NavBar() {
+
+const [name, setName] = useState('')
+const [opened, setOpened] = useState('')
+const [architect, setArchitect] = useState('')
+const [city, setCity] = useState('')
+const [image, setImage] = useState('')
+const [description, setDescription] =useState('')
+
+// let formData = { name, opened, architect, city, image, description }
+
+let formData = {
+	"name": name,
+	"opened": opened,
+	"architect": architect,
+	"city": city,
+	"image":  image ? image:'https://m.psecn.photoshelter.com/img-get/I0000AJkdFdU3w8E/s/1000?1646725782',
+	"description": description
+}
+
+function handleSubmit(e) {
+	e.preventDefault()
+
+	fetch("http://localhost:9292/buildings", {
+	method: "POST",
+	headers: {
+		"Content-Type": "application/json"
+	},
+	body: JSON.stringify(formData)
+	})
+	.then(res => res.json())
+	.then(newItem => {
+		console.log(newItem)
+	})
+}
+
+
+let form = (
+	<div className='form-container'>
+		<form className='form' onSubmit={handleSubmit}>
+			<label htmlFor='Form'>Building Name</label>
+			<input
+				className="name"
+				type="text"
+				id="name"
+				value={name}
+				placeholder="Building Name..."
+				onChange={e => setName(e.target.value)}
+			/>
+			<br></br>
+			<label htmlFor='Form'>Opened</label>
+			<input
+				className="name"
+				type="text"
+				id="opened"
+				value={opened}
+				placeholder="Year..."
+				onChange={e => setOpened(e.target.value)}
+			/>
+			<br></br>
+			<label htmlFor='Form'>Architect</label>
+			<input
+				className="name"
+				type="text"
+				id="architect"
+				value={architect}
+				placeholder="Architect Name..."
+				onChange={e => setArchitect(e.target.value)}
+			/>
+			<br></br>
+			<label htmlFor='Form'>City</label>
+			<input
+				className="name"
+				type="text"
+				id="city"
+				value={city}
+				placeholder="City..."
+				onChange={e => setCity(e.target.value)}
+			/>
+			<br></br>
+			<label htmlFor='Form'>Image</label>
+			<input
+				className="name"
+				type="text"
+				id="image"
+				value={image}
+				placeholder="Image URL..."
+				onChange={e => setImage(e.target.value)}
+			/>
+			<br></br>
+			<label htmlFor='Form'>Description</label>
+			<input
+				className="name"
+				type="text"
+				id="description"
+				value={description}
+				placeholder="Description..."
+				onChange={e => setDescription(e.target.value)}
+			/>
+			<br></br>
+			<input type="submit"></input>
+		</form>
+	</div>
+)
+console.log(description)
     return(
-        <>
-        <p>test</p>
-        </>
+        <div className="nav">
+            {form}
+        </div>
     )
 }
+
+
 
 export default NavBar;
