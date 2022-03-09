8899e91b6d3be4a6d4d7303e9dd3931f96c13c16
Chun Yi <8.5by11@gmail.com>
2022-03-09 09:48:36 -0500

    Added arch and city dropdowns

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-function NavBar({ archNames }) {
+function NavBar() {
 
 const [name, setName] = useState('')
 const [opened, setOpened] = useState('')
@@ -10,7 +10,6 @@ const [image, setImage] = useState('')
 const [description, setDescription] =useState('')
 
 // let formData = { name, opened, architect, city, image, description }
-
 
 let formData = {
 	"name": name,
@@ -24,32 +23,69 @@ let formData = {
 function handleSubmit(e) {
 	e.preventDefault()
 
-	const x = archNames.find(n => n.toLowerCase() === (architect.toLowerCase()))
-	
-	// if (x) {
-	// 	//Find the architect_id of the matching name and assign it to our formData obj
-	// }
-	// else {
-	// 	make a post request that creates a new architect with the name saved under 'x'
-	// 	and assign that architect to our new building
-	// }
-
-	// fetch("http://localhost:9292/buildings", {
-	// method: "POST",
-	// headers: {
-	// 	"Content-Type": "application/json"
-	// },
-	// body: JSON.stringify(formData)
-	// })
-	// .then(res => res.json())
-	// .then(newItem => {
-	// 	console.log(newItem)
-	// })
+	fetch("http://localhost:9292/buildings", {
+	method: "POST",
+	headers: {
+		"Content-Type": "application/json"
+	},
+	body: JSON.stringify(formData)
+	})
+	.then(res => res.json())
+	.then(newItem => {
+		console.log(newItem)
+	})
 }
-
 
 let form = (
 	<div className='form-container'>
+		<form>
+			<label>Architect:</label>
+                <select name="category" className="dropdown" value={architect} onChange={(e) => setArchitect(e.target.value)}>
+                    <option value="select">Select architect...</option>
+                    <option value="Albert Frey">Albert Frey</option>
+                    <option value="Alvar Aalto">Alvar Aalto</option>
+                    <option value="Arne Jacobsen">Arne Jacobsen</option>
+                    <option value="Charles and Ray Eames">Charles and Ray Eames</option>
+                    <option value="E. Stewart Williams">E. Stewart Williams</option>
+					<option value="Eero Saarinen">Eero Saarinen</option>
+					<option value="Frank Lloyd Wright">Frank Lloyd Wright</option>
+					<option value="Gerrit Rietveld">Gerrit Rietveld</option>
+					<option value="Ludwig Mies Van Der Rohe">Ludwig Mies Van Der Rohe</option>
+					<option value="Marcel Breuer">Marcel Breuer</option>
+					<option value="Oscar Niemeyer">Oscar Niemeyer</option>
+					<option value="Paul R Williams">Paul R Williams</option>
+					<option value="Pierre Koenig">Pierre Koenig</option>
+					<option value="Walter Gropius">Walter Gropius</option>
+					<option value="William Pereira">William Pereira</option>
+                </select>
+            <br></br>
+			<label>City:</label>
+                <select name="category" className="dropdown" value={city} onChange={(e) => setCity(e.target.value)}>
+                    <option value="select">Select city...</option>
+					<option value="Berlin">Berlin</option>
+					<option value="Bologna">Bologna</option>
+					<option value="Brasília">Brasília</option>
+					<option value="Curitiba">Curitiba</option>
+					<option value="Dessau-Roßlau">Dessau-Roßlau</option>
+					<option value="Dulles">Dulles</option>
+					<option value="Helsinki">Helsinki</option>
+					<option value="Irvine">Irvine</option>
+					<option value="Los Angeles">Los Angeles</option>
+					<option value="Mill Run">Mill Run</option>
+					<option value="New York City">New York City</option>
+					<option value="Oak Park">Oak Park</option>
+                    <option value="Palm Springs">Palm Springs</option>
+					<option value="Plano">Plano</option>
+                    <option value="Riola">Riola</option>
+                    <option value="Skovshoved">Skovshoved</option>
+					<option value="St. Louis">St. Louis</option>
+					<option value="Utrecht">Utrecht</option>
+					<option value="West Hollywood">West Hollywood</option>
+                </select>
+            <br></br>
+
+		</form>
+		
 		<form className='form' onSubmit={handleSubmit}>
 			<label htmlFor='Form'>Building Name</label>
 			<input
@@ -71,25 +107,6 @@ let form = (
 				onChange={e => setOpened(e.target.value)}
 			/>
 			<br></br>
-			<label htmlFor='Form'>Architect</label>
-			<input
-				className="name"
-				type="text"
-				id="architect"
-				value={architect}
-				placeholder="Architect Name..."
-				onChange={e => setArchitect(e.target.value)}
-			/>
-			<br></br>
-			<label htmlFor='Form'>City</label>
-			<input
-				className="name"
-				type="text"
-				id="city"
-				value={city}
-				placeholder="City..."
-				onChange={e => setCity(e.target.value)}
-			/>
 			<br></br>
 			<label htmlFor='Form'>Image</label>
 			<input
@@ -115,6 +132,7 @@ let form = (
 		</form>
 	</div>
 )
+console.log(description)
     return(
         <div className="nav">
             {form}
