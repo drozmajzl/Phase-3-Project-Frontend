b4d4607e7e7793ca0ebf13b54b48968101fd8651
Chun Yi <8.5by11@gmail.com>
2022-03-10 15:47:54 -0500

    Building card styling; wip

--- src/components/AgendaItem.js
+++ src/components/AgendaItem.js
@@ -4,10 +4,13 @@ function AgendaItem({handleDelete, name, city, opened, architect, architect_id, 
 
     return(
         <div className="building_container">
-            <h3>{name}</h3>
-            <p>Architect: {architect.full_name}</p>
-            <p>Completed: {opened}</p>
-            <p>City: {city.name}</p>
+            <div className="text_div">
+                <h4>{name}</h4>
+                <p>Architect: {architect.full_name}</p>
+                <p>Completed: {opened}</p>
+                <p>City: {city.name}</p>
+            </div>
+           
             <img className="card_image" src={image_url}></img>
             <button id="delete_button" onClick={()=>handleDelete(id)}>Delete</button>
         </div>
